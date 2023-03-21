88d967bdeaebf1c2c0fc35ffa000751d4bd3a452
JiananAlvin <jiananwang990@gmail.com>
2023-03-21 01:49:43 +0100

    calculate overlap of actual and expected successfully

--- ExecutionEngine/Program.cs
+++ ExecutionEngine/Program.cs
@@ -15,22 +15,19 @@ namespace ExecutionEngine
     internal class Program
     {
         // Uses kdTree to find the nearest neighbor pair between two lists of points
-        public static List<Tuple<int, int>> Compare(string expectedStates, string actualStates)
-        {   
-            List<List<int>> StatesExp = JsonConvert.DeserializeObject<List<List<int>>>(expectedStates);
-            List<List<int>> StatesAct = JsonConvert.DeserializeObject<List<List<int>>>(actualStates);
-
+        public static List<Tuple<int, int>> Compare(List<List<int>> StatesExp, List<List<int>> StatesAct)
+        {  
             KdTree<Coordinate> treeExp = BuildKdTree(StatesExp);
             KdTree<Coordinate> treeAct = BuildKdTree(StatesAct);
 
             List<Tuple<int, int>> pairs = new List<Tuple<int, int>>();
             for (int indexExp = 0; indexExp < treeExp.Count; indexExp++)
             {
-                var coordExp = new Coordinate(StatesExp[indexExp][0], StatesExp[indexExp][1]);
+                var coordExp = new Coordinate(StatesExp[indexExp][1], StatesExp[indexExp][2]);
                 var coordAct = treeAct.NearestNeighbor(coordExp);
                 // Console.WriteLine(coordAct.Coordinate);
-                // FindIndex takes O(n) time!!!! TODO: need modify!!!
-                int indexAct = StatesAct.FindIndex(b => b[0] == coordAct.X && b[1] == coordAct.Y);  // Finds the index of corresponding actual point
+                // FindIndex takes O
+                int indexAct = StatesAct.FindIndex(b => b[1] == coordAct.X && b[2] == coordAct.Y);  // Finds the index of corresponding actual point
                 var pair = new Tuple<int, int>(indexExp, indexAct);
                 pairs.Add(pair);
             }
@@ -44,8 +41,9 @@ namespace ExecutionEngine
             KdTree<Coordinate> treeS = new KdTree<Coordinate>(2);
             for (int i = 0; i < S.Count; i++)
             {
-                var point = new Coordinate(S[i][0], S[i][1]);
+                var point = new Coordinate(S[i][1], S[i][2]);
                 treeS.Insert(point);
+                // treeS.Insert(point, i);
             }
             return treeS;
         }
@@ -53,15 +51,36 @@ namespace ExecutionEngine
         static void Main(string[] args)
         {
 
-            string expectedS = "[[1, 2, 3, 7], [4, 5, 6, 7], [7, 8, 40, 9]]";
-            string actualS = "[[2, 3, 4, 6], [8, 9, 1, 9], [5, 6, 23, 45]]";
+            string expectedS = "[[100, 1, 2, 3, 7, 0, 0], [101, 4, 5, 6, 7, 0, 0], [102, 7, 8, 40, 9, 0, 0]]";
+            string actualS = "[[103, 2, 3, 4, 6, 0], [104, 8, 9, 1, 9, 0], [105, 5, 6, 23, 45, 0, 0]]";
 
-            List<Tuple<int, int>> pairs = Compare(expectedS, actualS);
+            List<List<int>> statesExp = JsonConvert.DeserializeObject<List<List<int>>>(expectedS);
+            List<List<int>> statesAct = JsonConvert.DeserializeObject<List<List<int>>>(actualS);
+
+            List<Tuple<int, int>> pairs = Compare(statesExp, statesAct);
 
             // Print pairs
-            foreach (Tuple<int, int> pair in pairs)
+/*            foreach (Tuple<int, int> pair in pairs)
             {
                 Console.WriteLine("A[{0}] - B[{1}]", pair.Item1, pair.Item2);
+            }*/
+
+            // Computes overlap
+            List<double> ious = new List<double>();
+            foreach (Tuple<int, int> pair in pairs)
+            {
+                Square squareExp = new Square(statesExp[pair.Item1][1], statesExp[pair.Item1][2], statesExp[pair.Item1][3], statesExp[pair.Item1][4]);
+                Square squareAct = new Square(statesAct[pair.Item2][1], statesAct[pair.Item2][2], statesAct[pair.Item2][3], statesAct[pair.Item2][4]);
+                double iou = squareExp.IoU(squareAct);
+                ious.Add(iou);
+            }
+            
+            // Print pairs and IoU
+            int i = 0;
+            foreach (Tuple<int, int> pair in pairs)
+            {
+                Console.WriteLine("A[{0}] - B[{1}], Iou is: {2}", pair.Item1, pair.Item2, ious[i]);
+                i++;
             }
         }
     }
---
+++ ExecutionEngine/Square.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecutionEngine
+{
+    internal class Square
+    {
+        internal double CenterX { get; }
+        internal double CenterY { get; }
+        internal double Width { get; }
+        internal double Height { get; }
+
+        internal Square(double centerX, double centerY, double width, double height)
+        {
+            CenterX = centerX;
+            CenterY = centerY;
+            Width = width;
+            Height = height;
+        }
+
+        // Calculate the area of the square
+        internal double Area()
+        {
+            return Width * Height;
+        }
+
+        // Calculate the intersection of two squares
+        internal double Intersection(Square s)
+        {
+            double x1 = CenterX - Width / 2; // Calculate the left edge of the square
+            double x2 = s.CenterX - s.Width / 2; // Calculate the left edge of the square
+            double y1 = CenterY - Height / 2; // Calculate the top edge of the square
+            double y2 = s.CenterY - s.Height / 2; // Calculate the top edge of the square
+
+            double xOverlap = Math.Max(0, Math.Min(x1 + Width, x2 + s.Width) - Math.Max(x1, x2));
+            double yOverlap = Math.Max(0, Math.Min(y1 + Height, y2 + s.Height) - Math.Max(y1, y2));
+
+            return xOverlap * yOverlap;
+        }
+
+        // Calculate the IoU of two squares
+        internal double IoU(Square s)
+        {
+            double intersection = Intersection(s);
+            double union = Area() + s.Area() - intersection;
+
+            return intersection / union;
+        }
+    }
+}
