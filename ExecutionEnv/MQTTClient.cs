152fe87d3ee9054b65d6f5e6a25d013049f929d4
JiananAlvin <jiananwang990@gmail.com>
2023-04-07 23:19:51 +0200

    add test json

--- ExecutionEnv/MQTTClient.cs
+++ ExecutionEnv/MQTTClient.cs
@@ -1,3 +1,4 @@
+using ExecutionEnv;
 using System.Text;
 // including the M2Mqtt Library
 using uPLibrary.Networking.M2Mqtt;
@@ -5,59 +6,53 @@ using uPLibrary.Networking.M2Mqtt.Messages;
 
 public class MQTTClient
 {
-
-    MqttClient client;
-    string clientId;
-    string name;
+    MqttClient Client;
+    string ClientId;
+    string Name;
     string BrokerAddress = "test.mosquitto.org";
 
     public MQTTClient(string name)
     {
-        client = new MqttClient(BrokerAddress);
+        Client = new MqttClient(BrokerAddress);
 
-        this.name = name;
+        this.Name = name;
 
         // register a callback-function (we have to implement, see below) which is called by the library when a message was received
-        client.MqttMsgPublishReceived += ClientMqttMsgPublishReceived;
+        Client.MqttMsgPublishReceived += ClientMqttMsgPublishReceived;
 
         // use a unique id as client id, each time we start the application
-        clientId = Guid.NewGuid().ToString();
+        ClientId = Guid.NewGuid().ToString();
 
-        client.Connect(clientId);
+        Client.Connect(ClientId);
     }
 
 
     protected void DisConnet()
     {
-        client.Disconnect();
-
+        Client.Disconnect();
     }
 
 
     public void Subscribe(string topic)
     {
         // whole topic
-        string finalTopic = $"/JNAlvin/test/{topic}";
+        string finalTopic = $"{topic}";
 
         // subscribe to the topic with QoS 2
-        client.Subscribe(new string[] { finalTopic }, new byte[] { 2 });   // we need arrays as parameters because we can subscribe to different topics with one call
+        Client.Subscribe(new string[] { finalTopic }, new byte[] { 2 });   // we need arrays as parameters because we can subscribe to different topics with one call
 
-        Console.WriteLine($"[{name}][Subscribte][{topic}]:Success");
-
+        Console.WriteLine($"[Subscribe][{topic}]:Success");
     }
-
 
     public void Publish(string topic, string content)
     {
         // whole topic
-        string finalTopic = $"/JNAlvin/test/{topic}";
+        string finalTopic = $"{topic}";
 
         // publish a message with QoS 2
-        client.Publish(finalTopic, Encoding.UTF8.GetBytes(content), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
-        Console.WriteLine($"[{name}][Publish][{topic}]:{content}");
-
+        Client.Publish(finalTopic, Encoding.UTF8.GetBytes(content), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+        Console.WriteLine($"[Publish][{topic}]:{content}");
     }
-
 
     // this code runs when a message was received
     public void ClientMqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
@@ -66,13 +61,10 @@ public class MQTTClient
 
         string topic = e.Topic;
 
-
-        if (topic.Equals("/JNAlvin/test/exe_feedback") && receivedMessage.Equals("ok"))
+        if (topic.Equals("exe/feedback") && receivedMessage.Equals("ok"))
         {
-            Program.executeCompletedFlag = true;
+            Tester.executeCompletedFlag = false;
         }
-
-
-        Console.WriteLine($"[{name}][Receive][{topic}]:{receivedMessage}");
+        Console.WriteLine($"[Receive][{topic}]:{receivedMessage}");
     }
 }
---
+++ ExecutionEnv/Tester.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecutionEnv
+{
+    internal class Tester
+    {
+        internal MQTTClient yolo = new MQTTClient("yolo");
+        internal MQTTClient executor = new MQTTClient("executor");
+        internal MQTTClient router = new MQTTClient("router");
+
+        internal string YOLO_RESULT_TOPIC = "yolo/act";
+        internal string ROUTER_RESULT_TOPIC = "router/exp";
+        internal string EXE_FEEDBACK_TOPIC = "exe/feedback";
+
+        internal string PATH_TO_YOLO = "../../../yolo.json";
+        internal string PATH_TO_ROUTER = "../../../router.json";
+
+
+
+        internal static bool executeCompletedFlag = false;
+
+        public Tester() 
+        {
+            // routerJsonArray = JArray.Parse(routerJsonText);
+            
+        }
+
+        public void ExecPublish()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Exec is runn.ing for round {i}");
+                // your code here
+
+                // TODO : 
+                Thread.Sleep(1000);
+                executor.Publish("exe/feedback", "ok");
+            }
+        }
+
+        public void RouterPublish()
+        {
+            string routerJsonText = File.ReadAllText(@$"{PATH_TO_ROUTER}");
+            JArray routerJsonArray = JArray.Parse(routerJsonText);
+
+            while (true)
+            {
+                router.Publish("router/exp", "4 TICK, expected states {electrodeID, tlX,tlY,width,height, Direction}");
+                while (true)
+                {
+                    if (executeCompletedFlag)
+                    {
+                        executeCompletedFlag = false;
+                        break;
+                    }
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        public void YoloPublish()
+        {
+            string yoloJsonText = File.ReadAllText(@$"{PATH_TO_YOLO}");
+            JArray yoloJsonArray = JArray.Parse(yoloJsonText);
+            
+            // Loop through each object in the array and print the "act" property
+            foreach (JObject obj in yoloJsonArray)
+            {
+                string actualStates = obj["act"].ToString();
+                yolo.Publish("yolo/act", $"{actualStates}");
+                Thread.Sleep(500);
+            }
+        }
+
+        public void Run()
+        {
+            executor.Subscribe(YOLO_RESULT_TOPIC);
+            executor.Subscribe(ROUTER_RESULT_TOPIC);
+            router.Subscribe(EXE_FEEDBACK_TOPIC);
+
+            new Thread(YoloPublish).Start();
+            new Thread(RouterPublish).Start();
+            new Thread(ExecPublish).Start();
+        }
+    }
+}
